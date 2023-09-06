2ec71a8b0a13f4cf40f6a852febd0bf93fb15890
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2023-09-07 02:15:02 +0900

    Add files via upload

---
+++ 05 Function/4 Lambda/lambda.py
@@ -0,0 +1,12 @@
+def sum(a,b):
+	c = a + b
+	return c
+total = sum(9,5)
+
+# Create Anonymous Function with Lambda
+# probably = lambda argument: print(argumen)
+probably = lambda x,y: x*y
+total = probably (9,5)
+print(total)
+print()
+print(9,5)
