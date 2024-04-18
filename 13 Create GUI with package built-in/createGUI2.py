e6e7143e299d24c0a3c2c35f618538c062daa9bc
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2024-04-18 16:24:09 +0700

    createGUI2.py

--- 13 Create GUI with package built-in/createGUI2.py
+++ 13 Create GUI with package built-in/createGUI2.py
@@ -3,7 +3,7 @@ import tkinter
 main_window = tkinter.Tk()
 
 def press_button():
-	label2 = tkinter.Label(main_window, text="Press Button")
+	label2 = tkinter.Label(main_window, text="Already Press The Button")
 
 label = tkinter.Label(main_window, text="Good Morning \n Create Simple GUI \n Use Python")
 button = tkinter.Button(main_window, text="press button", command= press_button)
