a5508efe8a6f448462c951de49a16fbce212ff5a
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-25 12:13:44 +0700

    Update function_argument1.py

--- 05 Function/2 Function Argument/function_argument1.py
+++ 05 Function/2 Function Argument/function_argument1.py
@@ -10,7 +10,7 @@ def Teacher(name,lesson):
 	print('Name   : ', name)
 	print('Lesson : ', lesson)
 
-Teacher(name='Beast',lesson='Read and Write')
+Teacher(name='Cowboy',lesson='Read and Write')
 print('\n')
 Teacher(lesson='Cooking', name='Kripik Khan')
 print('\n')
