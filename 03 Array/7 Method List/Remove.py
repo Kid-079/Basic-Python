33a58dee72b8e5dfa3461dbbf535e90a6652d847
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-25 13:30:08 +0700

    Update Remove.py

--- 03 Array/7 Method List/Remove.py
+++ 03 Array/7 Method List/Remove.py
@@ -1,10 +1,10 @@
-Array1 = [10,20,30,40,50,60,70]
-Array2 = ["A","B","C","D","E"]
+Array1 = [10,20,30,40,50,60,70,80,90,100]
+Array2 = ["Great","Spirit","Pretty","Relax","Struggle"]
 
 # Add List
 Data = Array1 + Array2
 print(Data)
 
 # Remove
-Data.remove("B")
+Data.remove("Relax")
 print(Data)
