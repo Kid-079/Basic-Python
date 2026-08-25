88c008d6f8e54d7022dd4064b0a9baf32b4fd9b3
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-25 13:36:14 +0700

    Update ChangeList1.py

--- 03 Array/4 Change List/ChangeList1.py
+++ 03 Array/4 Change List/ChangeList1.py
@@ -1,7 +1,7 @@
 # Change Content form List
 
-Array1 = [10,20,30,40,50,60,70]
-Array2 = [0,1,2,3,4,5,6]
+Array1 = [10,20,30,40,50,60,70,80,90,100]
+Array2 = [0,1,2,3,4,5,6,7,8]
 
 # Add List
 Data = Array1 + Array2
