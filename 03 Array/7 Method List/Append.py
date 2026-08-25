b128b3d75be8432e1b168d784fd675c9c101aada
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-25 13:21:42 +0700

    Update Append.py

--- 03 Array/7 Method List/Append.py
+++ 03 Array/7 Method List/Append.py
@@ -1,14 +1,14 @@
-Array1 = [10,20,30,40,50,60,70]
-Array2 = [0,1,2,3,4,5,6]
+Array1 = [10,20,30,40,50,60,70,80,90,100]
+Array2 = [0,1,2,3,4,5,6,7,8]
 
 # Create Multidimensional List
 Data = [Array1,Array2]
 print(Data)
 
 # Append
-Data2 = [1,2,3]
+Data2 = [5,15,25,35,45,55]
 Data.append(Data2)
-Data.append(["A","B","C"])
+Data.append(["Relax","Struggle","Enjoy","Spirit","Pretty"])
 Data.append(100)
 
 print(Data)
