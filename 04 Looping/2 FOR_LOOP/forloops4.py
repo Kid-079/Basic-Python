2d8cc5ab3e40d92307e146a490d2363a0d66c2ff
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-25 14:26:14 +0700

    Update forloops4.py

--- 04 Looping/2 FOR_LOOP/forloops4.py
+++ 04 Looping/2 FOR_LOOP/forloops4.py
@@ -1,7 +1,7 @@
 # Nested Loop
 String = ['Relax','ENJOY','pretty','Struggle','Spirit']
 Char = ['A','B','C','D','E']
-Number = [1,2,3,4,5,6]
+Number = [1,2,3,4,5,6,7,8,9,10]
 
 Array = [String,Char,Number]
 print(Array)
